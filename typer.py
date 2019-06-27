edf834974f0147bbe98306c7ed58e4e1623abcd3
ecly <ecly@mailbox.org>
2019-06-27 18:58:41 +0200

    add cmd line arguments and update wpm calculation

--- typer.py
+++ typer.py
@@ -1,20 +1,47 @@
+"""
+Minimal 10fastfingers like typing game using ncurses.
+Supports vocab files as arguments, as well as adjustable
+word length, game time and word frequency (if using sorted vocab).
+"""
 import argparse
 import time
 import random
 import curses
 
-WORDS = {"hello", "human", "another", "memes", "dank", "cat", "dog"}
+# Used for WPM calculation
+AVG_WORD_LENGTH = 5
 
-def sample_word():
-    return random.sample(WORDS, 1)[0]
-
+# pylint: disable=too-few-public-methods
 class Game:
-    def __init__(self, game_time=10):
-        self.game_time = game_time
-        self.next_words = [sample_word() for _ in range(10)]
+    """
+    Class encapsulating the Game.
+    Includes game stats, input management, game display.
+    """
+
+    def __init__(self, args):
+        self.word_generator = self._word_generator(args)
+        self.game_time = args.game_time
+        self.next_words = [self._get_word() for _ in range(10)]
         self.correct = []
         self.wrong = []
         self.input = ""
+
+    @staticmethod
+    def _word_generator(args):
+        words = []
+        for line in open(args.vocab):
+            word = line.strip()
+            if args.min_length <= len(word) <= args.max_length:
+                words.append(word)
+
+            if len(words) >= args.words:
+                break
+
+        while True:
+            yield random.choice(words)
+
+    def _get_word(self):
+        return next(self.word_generator)
 
     def _finish_word_event(self):
         target = self.next_words.pop(0)
@@ -23,7 +50,7 @@ class Game:
         else:
             self.wrong.append(target)
 
-        self.next_words.append(sample_word())
+        self.next_words.append(self._get_word())
         self.input = ""
 
     def _update_display(self, stdscr, time_left):
@@ -37,7 +64,7 @@ class Game:
             else:
                 stdscr.addstr(target_char, curses.color_pair(2))
 
-        stdscr.addstr(target[len(self.input):])
+        stdscr.addstr(target[len(self.input) :])
         stdscr.refresh()
 
     def _game_loop(self, stdscr):
@@ -69,22 +96,73 @@ class Game:
             time_left = new_time_left
 
     def play(self):
+        """Start typing game and print results to terminal"""
         curses.wrapper(self._game_loop)
 
         # print stats
         correct = len(self.correct)
         wrong = len(self.wrong)
         total = correct + wrong
-        wpm = 60 / self.game_time * correct
-        print(f"Words typed (correct/total): {correct}/{total}")
+        print(f"Word accuracy: {correct}/{total}")
+
+        correct_chars = len("".join(self.correct))
+        correct_words = correct_chars / AVG_WORD_LENGTH
+        wpm = 60 / self.game_time * correct_words
         print(f"WPM: {wpm}")
 
+
 def main():
+    """Parse arguments and start game based thereof"""
+    parser = argparse.ArgumentParser(
+        description="Start a minimal 10fastfingers-like typing game on the command line"
+    )
+    parser.add_argument(
+        "-v",
+        "--vocab",
+        type=str,
+        metavar="vocab-file-path",
+        default="vocab",
+        help="path to newline separated vocab file",
+    )
+    parser.add_argument(
+        "-t",
+        "--game_time",
+        type=int,
+        metavar="gametime-seconds",
+        default=60,
+        help="the duration in seconds of the typing game",
+    )
+    parser.add_argument(
+        "-min",
+        "--min_length",
+        type=int,
+        metavar="min-word-length",
+        default=2,
+        help="the minimum word length",
+    )
+    parser.add_argument(
+        "-max",
+        "--max_length",
+        type=int,
+        metavar="max-word-length",
+        default=10,
+        help="the maximum word length",
+    )
+    parser.add_argument(
+        "-w",
+        "--words",
+        type=int,
+        metavar="words-to-read",
+        default=1000,
+        help="the amount of words to read from vocab file",
+    )
+    args = parser.parse_args()
+    game = Game(args)
     try:
-        game = Game()
         game.play()
     except KeyboardInterrupt:
         pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

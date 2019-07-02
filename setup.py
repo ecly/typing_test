0e8835996e8f7d0db29ce8ae455ec0db9d404a7e
ecly <ecly@mailbox.org>
2019-07-02 12:25:41 +0200

    update version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='typing_test',
-    version='1.0',
+    version='0.1',
     description='Typing test in the terminal similar to 10fastfingers',
     license="MIT",
     long_description=long_description,

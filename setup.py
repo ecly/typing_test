e3d82e3304e98079214de27ce67171dd62e36d29
ecly <ecly@mailbox.org>
2019-07-02 12:24:20 +0200

    add setup.py and update readme

---
+++ setup.py
@@ -0,0 +1,29 @@
+# pylint: disable=missing-docstring
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='typing_test',
+    version='1.0',
+    description='Typing test in the terminal similar to 10fastfingers',
+    license="MIT",
+    long_description=long_description,
+    author='Emil Lynegaard',
+    author_email='ecly@mailbox.org',
+    url="http://www.github.com/ecly/typing_test",
+    packages=[],
+    install_requires=[],
+    scripts=['tt'],
+    keywords=[
+        "typing",
+        "typing test",
+        "10fastfingers",
+        "cmd",
+        "terminal",
+        "game",
+        "ncurses",
+        "curses",
+    ],
+)
